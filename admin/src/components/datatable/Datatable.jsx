45b868e93cb7690ac35e95c9d14529c0219d1611
Amir Hosein Nazari <czhablog@gmail.com>
2022-10-25 13:02:33 +0400

    feat: initialized the merging of the api and client

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -18,7 +18,7 @@ const Datatable = ({ columns }) => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`/${path}/${id}`);
+      await axios.delete(`http://localhost:5000/api/${path}/${id}`);
       setList(list.filter((item) => item._id !== id));
     } catch (err) {}
   };
